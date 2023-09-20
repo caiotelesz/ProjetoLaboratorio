d50f31318bbc326aba39ea0e5b2764be7326ebcd
Caio Teles <103958122+caiotelesz@users.noreply.github.com>
2023-09-20 15:46:19 -0300

    Finalizado

--- src/Main.java
+++ src/Main.java
@@ -24,7 +24,7 @@ public class Main {
                     posicaoPacientePrioritario = 0;
                     posicaoPacienteNormal = 0;
                 } if(opcao.equals(1)) {
-                    pessoa.enfileira(posicaoPacienteNormal, nome, true);
+                    pessoa.enfileira(posicaoPacientePrioritario, nome, true);
                     posicaoPacientePrioritario++;
                 } else if(opcao.equals(0)) {
                     pessoa.enfileira(posicaoPacienteNormal, nome, false);
